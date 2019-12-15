1fef391a9149ff11b84c7ec767d35d710a43aa21
yasmina-dev <yasmina.ikhlef91@gmail.com>
2019-12-15 12:51:56 +0100

    create entity user

---
+++ src/Entity/User.php
@@ -0,0 +1,70 @@
+<?php
+
+
+namespace App\Entity;
+
+
+class User
+{
+    private $id;
+    private $nom;
+    private $prenom;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     * @return User
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     * @return User
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param mixed $prenom
+     * @return User
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+        return $this;
+    }
+
+
+
+
+}
